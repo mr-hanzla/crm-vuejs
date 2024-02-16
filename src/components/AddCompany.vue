57bca143faeccf838230514346cd07143abe992b
Mr Hanzla <mr.hanzla101@gmail.com>
2024-02-16 09:04:46 +0500

    add components for company registration & showing companies

---
+++ src/components/AddCompany.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { ref } from 'vue';
+
+const companyName = ref('');
+const companyUrl = ref('');
+const companyLogoUrl = ref('');
+const companyDescription = ref('');
+
+function addCompany() {
+  const companyData = {
+    companyName: companyName.value,
+    companyUrl: companyUrl.value,
+    companyLogoUrl: companyLogoUrl.value,
+    companyDescription: companyDescription.value,
+  }
+  
+  let companyList = JSON.parse(localStorage.getItem('companyList'));
+  if (!companyList) {
+    companyList = []
+  }
+  companyList.push(companyData);
+
+  console.log(JSON.stringify(companyData));
+  localStorage.setItem('companyList', JSON.stringify(companyList));
+  localStorage.setItem('temp', JSON.stringify(companyData));
+}
+</script>
+
+<template>
+  <div class="register-company-form">
+    <v-img class="mx-auto my-6" max-width="500"
+      src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg"></v-img>
+
+    <v-card class="mx-auto pa-12 pb-8" elevation="15" max-width="800" rounded="lg">
+      <v-text-field v-model="companyName" clearable label="Company Name" placeholder="Sample Name"
+        variant="outlined"></v-text-field>
+      <v-text-field v-model="companyUrl" clearable label="Website URL" placeholder="https//www.sample.com"
+        variant="outlined"></v-text-field>
+      <v-text-field v-model="companyLogoUrl" clearable label="Logo URL" placeholder="https//www.sample.com/logo.jpg"
+        variant="outlined"></v-text-field>
+
+      <v-textarea v-model="companyDescription" label="Description" placeholder="Tell about the company"
+        variant="outlined"></v-textarea>
+
+      <v-btn @click="addCompany" class="mx-auto mb-8" size="large" color="black">Add Company</v-btn>
+      <!-- <v-card-text class="text-center">
+        <a class="text-blue text-decoration-none" href="#" rel="noopener noreferrer" target="_blank">
+          Add Company <v-icon icon="mdi-chevron-right"></v-icon>
+        </a>
+      </v-card-text> -->
+    </v-card>
+  </div>
+</template>
+
+<style scoped>
+/* .register-comapny-form {
+  border: 4px solid red;
+  margin-bottom: 50%;
+} */</style>
---
+++ src/components/Company.vue
@@ -0,0 +1,28 @@
+<script setup>
+</script>
+
+<template>
+  <h1>Company Data</h1>
+  <v-list lines="two" style="margin-left: 30%;">
+    <v-list-item v-for="n in 3" :key="n" :title="'Item ' + n"
+      subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit"></v-list-item>
+    <v-list-item>
+      <v-sheet class="d-flex align-center justify-center flex-wrap text-center mx-auto px-4" elevation="4" height="250"
+        rounded max-width="900" width="100%">
+        <div>
+          <h2 class="text-h4 font-weight-black text-orange">Congratulations!</h2>
+
+          <div class="text-h5 font-weight-medium mb-2">
+            You are officially a part of the Vuetify Community!
+          </div>
+
+          <p class="text-body-2 mb-4">
+            Please head over to your inbox/spam or others folder to find our verificaiton email.
+          </p>
+
+          <v-btn variant="text" color="orange">Go to Login</v-btn>
+        </div>
+      </v-sheet>
+    </v-list-item>
+  </v-list>
+</template>
