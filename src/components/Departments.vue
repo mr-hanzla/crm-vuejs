e3e5baad90ed3ec6ed4ba1e6d59ce25ac5a426ae
Mr Hanzla <mr.hanzla101@gmail.com>
2024-02-20 22:50:52 +0500

    add new components for department and users

---
+++ src/components/Departments.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { onMounted } from 'vue';
+import { reactive } from 'vue';
+
+const departmentList = reactive([
+  { id: 1, name: 'Admin' },
+  { id: 2, name: 'Sales' },
+  { id: 3, name: 'Technical' },
+  { id: 4, name: 'Marketing' },
+  { id: 5, name: 'HR' },
+  { id: 6, name: 'Office Staff' },
+])
+
+onMounted(() => {
+  console.log('Departments are mounted!')
+})
+</script>
+
+<template>
+  <div style="margin-left: 50%;">
+    <h1>Departments</h1>
+    <h1>Departments</h1>
+    <h1>Departments</h1>
+    <h1>Departments</h1>
+    <h1>Departments</h1>
+  </div>
+</template>
+
+<style></style>
---
+++ src/components/Users.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+const users = [
+  { id: 1, username: 'hanzla', password: 'abc123', role: 'superuser', companies: [] },
+  { id: 2, username: 'mashhood', password: 'abc123', role: 'admin', companies: [1, 2] },
+  { id: 3, username: 'kayani', password: 'abc123', role: 'admin', companies: [3] },
+  { id: 4, username: 'testuser', password: 'abc123', role: 'admin', companies: [4, 5] },
+]
+
+localStorage.setItem('users', JSON.stringify(users));
+
+
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+</style>
+
